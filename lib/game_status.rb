fe4b1f8944be3edf161f785ed58c7816a5443247
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-07-31 02:21:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,6 @@ end
 WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
 
 def won?(board)
-  
   WIN_COMBINATIONS.each do |combo|
     if (board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]) && board[combo[0]] != " "
       return combo
